60087392ae355ab405b0a94a1718ca72ff68fc5a
Shubham Sharma <mr.shubhshar@gmail.com>
2017-05-17 00:49:20 +0530

    bugs squashed

---
+++ app.js
@@ -0,0 +1,26 @@
+particlesJS("snowfall", {
+    "particles": {
+        "number": {
+            "value": 100
+        },
+        "shape": {
+            "type": "circle"
+        },
+        "size": {
+            "value": 5,
+            "random": true
+        },
+        "line_linked": {
+            "enable": true
+        },
+        "move": {
+            "enable": true, //default
+            "speed": 2,
+            "direction": "random",
+            "straight": false
+        }
+    },
+    "interactivity": {
+
+    }
+});
