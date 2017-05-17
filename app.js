4b017f1610e6a43232004dec6f69b3827b611def
Shubham Sharma <mr.shubhshar@gmail.com>
2017-05-17 13:59:27 +0530

    tweaked more

--- app.js
+++ app.js
@@ -4,23 +4,27 @@ particlesJS("snowfall", {
             "value": 100
         },
         "shape": {
-            "type": "circle"
+            "type": "star"
         },
         "size": {
-            "value": 5,
+            "value": 7,
             "random": true
         },
         "line_linked": {
-            "enable": true
+            "enable": false
         },
         "move": {
             "enable": true, //default
-            "speed": 2,
-            "direction": "random",
-            "straight": false
+            "speed": 1.2,
+            "direction": "bottom",
+            "straight": false //default
         }
     },
     "interactivity": {
-
+        "events": {
+            "onhover": {
+                "enable": false
+            }
+        }
     }
 });
